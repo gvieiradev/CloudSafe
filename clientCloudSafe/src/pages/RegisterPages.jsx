c37ec99f9db510aef16846acb24572c2607ff577
unknown <ing.gabrielvieira@gmail.com>
2023-10-10 22:49:05 -0400

    inicio de desarrollo frontEnd de registro

---
+++ clientCloudSafe/src/pages/RegisterPages.jsx
@@ -0,0 +1,17 @@
+function RegisterPage(){
+    return(
+        <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+            <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5">
+                <div className="w-1/2">
+                    <h2 className="font-bold text-2xl">Register</h2>
+                </div>
+
+                <div className="w-1/2">
+                    <img className="rounded-2xl" src="https://i.ibb.co/X343g7v/fce45d68d7ac59820e2c4d2f01d6fa20.png" alt="fce45d68d7ac59820e2c4d2f01d6fa20" border="0"/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default RegisterPage;
