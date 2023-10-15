b9b45c90a82af5e02b3fcfd6d9fd0aef3eaa80e5
unknown <ing.gabrielvieira@gmail.com>
2023-10-15 00:22:39 -0400

    finalizacion frontEnd registro de usuarios

--- clientCloudSafe/src/pages/RegisterPages.jsx
+++ clientCloudSafe/src/pages/RegisterPages.jsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth.js";
+
 function RegisterPage(){
+
+    const {register, handleSubmit} = useForm();
+    const onSubmit = handleSubmit(async (values) =>{
+        console.log(values);
+        const res = await registerRequest(values)
+        console.log(res)
+    })
+
     return(
         <section className="bg-gray-50 min-h-screen flex items-center justify-center">
             <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
@@ -6,20 +17,20 @@ function RegisterPage(){
                     <h2 className="font-bold text-2xl text-[#1F0061]">Register</h2>
                     <p className="text-sm mt-2 text-[#1F0061]">Store and share files. Everything with CloudSafe</p>
 
-                    <form className="flex flex-col gap-4" action="">
+                    <form className="flex flex-col gap-4" onSubmit={onSubmit}>
                         <div className="relative">
-                            <input className="p-2 mt-3 rounded-xl border w-full" type="text" name="fullName" placeholder="Full name"/>
-                            <input className="p-2 mt-2 rounded-xl border w-full" type="text" name="userName" placeholder="Username"/>
-                            <input className="p-2 mt-2 rounded-xl border w-full" type="email" name="email" placeholder="Email"/>
-                            <input className="p-2 mt-2 rounded-xl border w-full" type="password" name="password" placeholder="Password"/>
+                            <input className="p-2 mt-3 rounded-xl border w-full" type="text" placeholder="Full name" {...register("name", {required:true})}/>
+                            <input className="p-2 mt-2 rounded-xl border w-full" type="text" placeholder="Username" {...register("username", {required:true})}/>
+                            <input className="p-2 mt-2 rounded-xl border w-full" type="email" placeholder="Email" {...register("email", {required:true})}/>
+                            <input className="p-2 mt-2 rounded-xl border w-full" type="password" placeholder="Password" {...register("password", {required:true})}/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute -mt-5 right-3 -translate-y-1/2" viewBox="0 0 16 16">/
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                             </svg>
-                            <input className="p-2 mt-2 rounded-xl border w-full" type="password" name="confirmPassword" placeholder="Confirm password"/>
+                            <input className="p-2 mt-2 rounded-xl border w-full" type="password" placeholder="Confirm password" {...register("confirmPassword", {required:true})}/>
                             
                         </div>
-                        <button className="bg-[#1F0061] rounded-xl text-white py-2 hover:scale-105 duration-300">Register</button>
+                        <button className="bg-[#1F0061] rounded-xl text-white py-2 hover:scale-105 duration-300" type="submit">Register</button>
                     </form>
 
                     <div className="mt-4 grid grid-cols-3 items-center text-gray-400">
