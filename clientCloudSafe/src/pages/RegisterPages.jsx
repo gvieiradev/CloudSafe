47f365840c464bdd26601ea53691b13124884b3b
unknown <ing.gabrielvieira@gmail.com>
2023-10-16 22:19:06 -0400

    contexto usuario

--- clientCloudSafe/src/pages/RegisterPages.jsx
+++ clientCloudSafe/src/pages/RegisterPages.jsx
@@ -1,14 +1,21 @@
 import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth.js";
+import { useAuth } from "../context/authContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage(){
 
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState:{errors}} = useForm();
+    const {signup, isAuthenticated, errors:registerError} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(isAuthenticated) navigate("/tasks");
+    }, [isAuthenticated])
+
     const onSubmit = handleSubmit(async (values) =>{
-        console.log(values);
-        const res = await registerRequest(values)
-        console.log(res)
-    })
+        signup(values);    
+    });
 
     return(
         <section className="bg-gray-50 min-h-screen flex items-center justify-center">
@@ -16,19 +23,29 @@ function RegisterPage(){
                 <div className="sm:w-1/2 px-16">
                     <h2 className="font-bold text-2xl text-[#1F0061]">Register</h2>
                     <p className="text-sm mt-2 text-[#1F0061]">Store and share files. Everything with CloudSafe</p>
-
+                    {
+                        registerError.map((error, i) => (
+                            <div className="bg-red-500 p-2 text-white" key={i}>
+                                {error}
+                            </div>
+                        ))
+                    }
                     <form className="flex flex-col gap-4" onSubmit={onSubmit}>
                         <div className="relative">
                             <input className="p-2 mt-3 rounded-xl border w-full" type="text" placeholder="Full name" {...register("name", {required:true})}/>
+                            {errors.name && <p className="text-red-500">Username is required</p>}
                             <input className="p-2 mt-2 rounded-xl border w-full" type="text" placeholder="Username" {...register("username", {required:true})}/>
+                            {errors.username && <p className="text-red-500">Name is required</p>}
                             <input className="p-2 mt-2 rounded-xl border w-full" type="email" placeholder="Email" {...register("email", {required:true})}/>
+                            {errors.email && <p className="text-red-500">Email is required</p>}
                             <input className="p-2 mt-2 rounded-xl border w-full" type="password" placeholder="Password" {...register("password", {required:true})}/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute -mt-5 right-3 -translate-y-1/2" viewBox="0 0 16 16">/
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                             </svg>
+                            {errors.password && <p className="text-red-500">Password is required</p>}
                             <input className="p-2 mt-2 rounded-xl border w-full" type="password" placeholder="Confirm password" {...register("confirmPassword", {required:true})}/>
-                            
+                            {errors.confirmPassword && <p className="text-red-500">Confirm password is required</p>}
                         </div>
                         <button className="bg-[#1F0061] rounded-xl text-white py-2 hover:scale-105 duration-300" type="submit">Register</button>
                     </form>
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,10 @@ export const register = async (req, res) =>{
     const {name,username,email,password,confirmPassword} = req.body
 
     try {
+        const userFound = await User.findOne({email});
+        if(userFound)
+            return res.status(400).json(["The email already exists"]);
+    
         const passwordHash = await bcrypt.hash(password, 10);
         const newUser = new User({name, username, email, password:passwordHash, confirmPassword});
 
